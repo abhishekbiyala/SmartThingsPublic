3c98f954f45f4baaaec360aee5aabb502ff06365
Abhishek Sharma <abhishekbiyala@gmail.com>
2015-09-28 21:55:24 -0700

    Updated

--- smartapps/abhishekbiyala/left-it-open-when-armed.src/left-it-open-when-armed.groovy
+++ smartapps/abhishekbiyala/left-it-open-when-armed.src/left-it-open-when-armed.groovy
@@ -1,5 +1,5 @@
 /**
- *  Copyright 2015 SmartThings
+ *  Copyright 2015 Abhishek Sharma
  *
  *  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
  *  in compliance with the License. You may obtain a copy of the License at:
@@ -10,10 +10,9 @@
  *  on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License
  *  for the specific language governing permissions and limitations under the License.
  *
- *  Left It Open
+ *  Left It Open When Armed
  *
- *  Author: SmartThings
- *  Date: 2013-05-09
+ *  Author: Abhishek Sharma
  */
 definition(
     name: "Left It Open When Armed",
